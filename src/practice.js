4f5950fb446b76ff6f9170fb075c6b4e03255c40
UnbenchTheBench <131629341+UnbenchTheBench@users.noreply.github.com>
2025-09-20 17:10:32 -0500

    practice.js

---
+++ src/practice.js
@@ -0,0 +1,32 @@
+
+/*
+import useGeolocation from "./hooks/useGeolocation";
+
+const { coords, error } = useGeolocation();
+if (!coords) {
+    console.error("Geolocation not available");
+}*/
+
+const {lat, lon } = coords || {10, 20};
+
+async function getWeatherData(days) {
+    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=3e0a80a10d8d441799d175849252009&q=20,%2020&days=${days}&q=${lat},${lon}`);
+
+    if (!response.ok) {
+        throw new Error("Failed to fetch weather data");
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+async function main() {
+    try {
+        const date = await getWeatherData(3);
+        console.log(date);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+main();
